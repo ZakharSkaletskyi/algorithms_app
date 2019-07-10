41be80146c4fe2ef9ab4b17b732483a551cd24d2
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-07-10 15:20:08 +0300

    initial commit for 2 methods

---
+++ src/main/java/lv_427/Logic/nazar_vladyka/LongestSubsequence.java
@@ -0,0 +1,9 @@
+package lv_427.Logic.nazar_vladyka;
+
+import lv_427.Logic.AlgorithmExecutor;
+
+public class LongestSubsequence implements AlgorithmExecutor {
+    public void execute() {
+        
+    }
+}
