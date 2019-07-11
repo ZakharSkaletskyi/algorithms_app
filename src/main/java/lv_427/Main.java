feb7d28145d87b8644de680ab95af88b1123d44d
MarianMilian <milanmarian@mail.ru>
2019-07-11 16:41:47 +0300

    Merge branch 'master' of C:\Users\Marian\IdeaProjects\algorithms_app with conflicts.

--- src/main/java/lv_427/Main.java
+++ src/main/java/lv_427/Main.java
@@ -2,6 +2,8 @@ package lv_427;
 
 import java.util.Scanner;
 import lv_427.logic.AlgorithmExecutor;
+import lv_427.logic.marian_milian.IntrestingRow;
+import lv_427.logic.marian_milian.ModificationFibonacci;
 import lv_427.logic.kateryna_horokh.PalindromicSubsequence;
 import lv_427.logic.nazar_stasyuk.Fibonacci;
 import lv_427.logic.roman_zahorui.WaysTileFloor;
@@ -73,8 +75,11 @@ public class Main {
           algorithmExecutor = new Fibonacci();
           break;
         case 2:
+          algorithmExecutor=new ModificationFibonacci();
           break;
         case 3:
+            algorithmExecutor=new IntrestingRow();
+
           break;
         case 4:
           break;
--- src/main/java/lv_427/logic/marian_milian/ModificationFibonacci.java
+++ src/main/java/lv_427/logic/marian_milian/ModificationFibonacci.java
@@ -1,8 +1,38 @@
 package lv_427.logic.marian_milian;
 
+import java.util.ArrayList;
+import java.util.List;
 import lv_427.logic.AlgorithmExecutor;
 
 public class ModificationFibonacci implements AlgorithmExecutor {
+  private final int LENGTH_OF_FIBONACCI_ROW =51;
+  private List<Integer> sequenceFibonacciRow;
 
-  public void execute() {}
+  public ModificationFibonacci() {
+    sequenceFibonacciRow = new ArrayList<Integer>();
+    generateFibonacciSequence();
+    System.out.println(sequenceFibonacciRow.get(6 - 1));
+    System.out.print(sequenceFibonacciRow);
+    System.out.println(" ");
+    System.out.println(sequenceFibonacciRow.size());
+  }
+
+  public void execute() {
+
+  }
+
+  private void generateFibonacciSequence() {
+    int sum;
+    sequenceFibonacciRow.add(1);
+    sequenceFibonacciRow.add(1);
+    sequenceFibonacciRow.add(1);
+    for (int i = 3; i < LENGTH_OF_FIBONACCI_ROW - 3; i++) {
+      sum = sequenceFibonacciRow.get(i - 1) + sequenceFibonacciRow.get(i - 3);
+      if (sum < 0) {
+        break;
+      }
+
+      sequenceFibonacciRow.add(sum); //
+    }
+  }
 }
