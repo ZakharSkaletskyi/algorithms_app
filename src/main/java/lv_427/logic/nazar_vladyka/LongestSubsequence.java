42759eb02e5f5b9be55337782f39ad8498fb2353
Nazar Vladyka <nazarvladyka00@gmail.com>
2019-07-11 16:48:47 +0300

    logic for PathWithoutCrossing

--- src/main/java/lv_427/logic/nazar_vladyka/LongestSubsequence.java
+++ src/main/java/lv_427/logic/nazar_vladyka/LongestSubsequence.java
@@ -2,6 +2,85 @@ package lv_427.logic.nazar_vladyka;
 
 import lv_427.logic.AlgorithmExecutor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.System.err;
+
+/**
+ * Longest subsequence with difference one. The goal: To find how many numbers we have with
+ * difference one in the maximum sub sequence
+ *
+ * @author Nazar Vladyka
+ * @version 1.0
+ * @since 2019-07-11
+ */
 public class LongestSubsequence implements AlgorithmExecutor {
-  public void execute() {}
+  private Scanner sc;
+
+  public LongestSubsequence() {
+    this.sc = new Scanner(System.in);
+  }
+
+  public static void main(String[] args) {
+    new LongestSubsequence().execute();
+  }
+
+  public void execute() {
+    System.out.println("Hello! This is \"Longest subsequence with difference one\"");
+    System.out.print("Please send me the numbers: ");
+
+    boolean isInputWrong = true;//by default
+
+    while (isInputWrong) {
+      String input = sc.nextLine();
+      String[] arrayS = input.split(" ");
+
+      if (arrayS.length > 0) {
+        isInputWrong = false;
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        for (String s : arrayS) {
+          try {
+            numbers.add(Integer.parseInt(s));
+          } catch (NumberFormatException e) {
+            err.println(e);
+            isInputWrong = true;
+            break;
+          }
+        }
+
+        if (isInputWrong) {
+          System.out.print("Try again: ");
+        } else {
+          System.out.println("Result: " + find(numbers));
+        }
+
+      } else {
+        System.out.print("Something wrong(\nTry again: ");
+      }
+    }
+  }
+
+  public int find(List<Integer> numbers) {
+    int maxSubsequence = 0;
+    int currentSubsequence = 1;
+
+    for (int i = 0; i < numbers.size() - 1; i++) {
+      if ((numbers.get(i + 1) - numbers.get(i)) == 1) {
+        currentSubsequence++;
+      } else {
+        if (currentSubsequence > maxSubsequence) {
+          maxSubsequence = currentSubsequence;
+        }
+        currentSubsequence = 1;
+      }
+    }
+
+    if (currentSubsequence > maxSubsequence) {
+      maxSubsequence = currentSubsequence;
+    }
+    return maxSubsequence;
+  }
 }
--- src/main/java/lv_427/logic/nazar_vladyka/PathsWithoutCrossing.java
+++ src/main/java/lv_427/logic/nazar_vladyka/PathsWithoutCrossing.java
@@ -4,4 +4,31 @@ import lv_427.logic.AlgorithmExecutor;
 
 public class PathsWithoutCrossing implements AlgorithmExecutor {
   public void execute() {}
+
+  static int catalanDP(int n) {
+
+    int[] catalan = new int[n + 1];
+
+    catalan[0] = catalan[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      catalan[i] = 0;
+      for (int j = 0; j < i; j++) catalan[i] += catalan[j] * catalan[i - j - 1];
+    }
+
+    return catalan[n];
+  }
+
+  static int countWays(int n) {
+    if (n < 1) {
+      System.out.println("Invalid");
+      return 0;
+    }
+
+    return catalanDP(n / 2);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(countWays(6) + " ");
+  }
 }
