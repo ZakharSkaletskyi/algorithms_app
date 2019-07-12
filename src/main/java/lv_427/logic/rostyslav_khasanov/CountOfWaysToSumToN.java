7b67768c27f2199bdee4451f7a68b1a5e54d5926
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-07-12 21:31:14 +0300

    Add javadoc

--- src/main/java/lv_427/logic/rostyslav_khasanov/CountOfWaysToSumToN.java
+++ src/main/java/lv_427/logic/rostyslav_khasanov/CountOfWaysToSumToN.java
@@ -4,33 +4,73 @@ import lv_427.logic.TaskExecutor;
 
 import java.util.Scanner;
 
+/**
+ * Ways to sum to N using array elements with repetition
+ *
+ * <p>The goal: To find how many ways we have to represent one number with a given array of numbers
+ *
+ * @author Rostyslav Khasanov
+ * @version 1.0
+ */
 public class CountOfWaysToSumToN implements TaskExecutor {
 
+  /** Executable method. */
   public void execute() {
-
+    boolean check = true;
     Scanner sc = new Scanner(System.in);
-    String strArr[];
-    int numbers[];
-    String str = "";
+    String str;
     int number = 0;
-    int m = 0;
+    /** The length of array numbers */
+    int m;
+    /** Array for string elements */
+    String strArr[];
+    /** Array with integers, which consist elements of strArr after parse */
+    int numbers[];
 
     System.out.println("Enter array of numbers like '1 5 2 12 1': \n");
-    try {
+    for (; ; ) {
       str = sc.nextLine();
-    } catch (Exception e) {
-      e.printStackTrace();
+      strArr = str.split(" ");
+      numbers = new int[strArr.length];
+      try {
+        for (int i = 0; i < strArr.length; i++) {
+          numbers[i] = Integer.parseInt(strArr[i]);
+        }
+        check = false;
+      } catch (Exception e) {
+        System.out.println("Only numbers is array like '1 22 4 3' !");
+      }
+      if (check == false) {
+        break;
+      }
     }
-    number = Integer.parseInt(sc.nextLine());
-    strArr = str.split(" ");
-    m = strArr.length;
-    numbers = new int[strArr.length];
-    for (int i = 0; i < strArr.length; i++) {
-      numbers[i] = Integer.parseInt(strArr[i]);
+    check = true;
+    m = numbers.length;
+    System.out.println("Enter positive number :");
+    for (; ; ) {
+      try {
+        number = Integer.parseInt(sc.nextLine());
+        check = false;
+      } catch (Exception e) {
+        System.out.println("Enter just one positive number!");
+      }
+      if (check == false) {
+        break;
+      }
     }
+
     System.out.println(countWays(numbers, number, m));
   }
 
+  /**
+   * Method is for find how many ways we have to represent one number with a given array of numbers
+   *
+   * @param m is length of array numbers.
+   * @param n is one entered positive number
+   * @param arr is array of numbers.
+   * @return returns a number that means how many ways we have to represent one number with a given
+   *     array of numbers integers.
+   */
   public int countWays(int[] arr, int n, int m) {
     int mas[] = arr;
     int count[] = new int[n + 1];
