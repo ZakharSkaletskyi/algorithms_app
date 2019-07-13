3061e34cce8c0f4eb99ecc66b6831fd5c560d0ab
ZakharSk <sklzakhar@gmail.com>
2019-07-13 19:05:15 +0300

    add exceptions for WaysToCoverIn3steps

--- src/main/java/lv_427/logic/zakhar_skaletskyi/WaysToCoverIn3steps.java
+++ src/main/java/lv_427/logic/zakhar_skaletskyi/WaysToCoverIn3steps.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import lv_427.logic.TaskExecutor;
 
 public class WaysToCoverIn3steps implements TaskExecutor {
-	private int totalCountOfWays;
+	private long totalCountOfWays;
 	private int distance;
 	private Scanner sc;
 
@@ -15,15 +15,16 @@ public class WaysToCoverIn3steps implements TaskExecutor {
 
 	public void execute() {
 		totalCountOfWays = 1; // all distance we cover by 1 step
-		System.out.println("Please input distance");
-		distance = sc.nextInt();
+		distance = getDistance(sc);
 		totalCountOfWays += findWays(distance, 3, 0, 0, 0, 0);
-		System.out.println("Total number of ways to cover the distance = " + totalCountOfWays + "\n");
+		System.out.println("Total number of ways to cover the distance = " + totalCountOfWays);
+		System.out.println("To return to menu just press Enter");
+		sc.nextLine();
 	}
 
-	private int findWays(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
-		int totalCountOfWays = 0;
-		if (stepDistance == 3) {
+	private long findWays(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
+		long totalCountOfWays = 0;
+		if (stepDistance == 3) {	//start calculate for 3 steps and than for less steps
 			totalCountOfWays += getTwoStepsCount(distance, 2, passedSteps, step1, step2, step3);
 			totalCountOfWays += getThreeStepsCount(distance, 3, passedSteps, step1, step2, step3);
 		}
@@ -38,20 +39,22 @@ public class WaysToCoverIn3steps implements TaskExecutor {
 		}
 		return totalCountOfWays;
 	}
-	public int countWays(int passedSteps, int step1, int step2, int step3) {
-		int factorial = getFactorial(passedSteps) / getFactorial(step1) / getFactorial(step2) / getFactorial(step3);
+
+	public long countWays(int passedSteps, int step1, int step2, int step3) {
+		long factorial = getFactorial(passedSteps) / getFactorial(step1) / getFactorial(step2) / getFactorial(step3);
 		return factorial;
 	}
 
-	public int getOneStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
-		int twoStepsCount = 0;
-		step1+= distance;
+	public long getOneStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
+		long twoStepsCount = 0;
+		step1 += distance;
 		passedSteps += distance;
 		twoStepsCount += countWays(passedSteps, step1, step2, step3);
 		return twoStepsCount;
 	}
-	public int getTwoStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
-		int twoStepsCount = 0;
+
+	public long getTwoStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
+		long twoStepsCount = 0;
 		for (int coveredDistanceWith2Steps = 2; coveredDistanceWith2Steps <= distance; coveredDistanceWith2Steps += 2) {
 			passedSteps++;
 			step2++;
@@ -59,8 +62,9 @@ public class WaysToCoverIn3steps implements TaskExecutor {
 		}
 		return twoStepsCount;
 	}
-	public int getThreeStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
-		int threeStepsCount = 0;
+
+	public long getThreeStepsCount(int distance, int stepDistance, int passedSteps, int step1, int step2, int step3) {
+		long threeStepsCount = 0;
 		for (int coveredDistanceWith3Steps = 3; coveredDistanceWith3Steps <= distance; coveredDistanceWith3Steps += 3) {
 			passedSteps++;
 			step3++;
@@ -68,11 +72,32 @@ public class WaysToCoverIn3steps implements TaskExecutor {
 		}
 		return threeStepsCount;
 	}
-	public static int getFactorial(int n) {
-		int r = 1;
+
+	public static long getFactorial(int n) {
+		long r = 1;
 		for (int i = 1; i <= n; i++) {
 			r *= i;
 		}
 		return r;
+	}
+
+	public int getDistance(Scanner sc) {
+		boolean check = true;
+		int distance = 0;
+		System.out.println("Please enter a distance less than 30");
+		while (check) {
+			try {
+				distance = Integer.parseInt(sc.nextLine());
+				if ((distance > 0) && (distance <=30)) {
+					check = false;
+				} else {
+					check = true;
+					System.out.println("Please enter just one positive number less than 30!");
+				}
+			} catch (Exception e) {
+				System.out.println("Please enter just one positive number less than 30!");
+			}
+		}
+		return distance;
 	}
 }
