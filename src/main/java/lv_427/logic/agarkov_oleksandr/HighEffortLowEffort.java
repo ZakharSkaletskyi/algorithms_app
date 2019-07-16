10720f38aec0b911a8d5e83b9e504454ac2b5b6f
fsociety01010 <alexbmx@mail.ua>
2019-07-16 12:43:30 +0300

    encapsulation of methods

--- src/main/java/lv_427/logic/agarkov_oleksandr/HighEffortLowEffort.java
+++ src/main/java/lv_427/logic/agarkov_oleksandr/HighEffortLowEffort.java
@@ -12,57 +12,89 @@ import lv_427.logic.TaskExecutor;
  * @version 1.0
  */
 public class HighEffortLowEffort implements TaskExecutor {
-  /** Scanner for reading from console. */
-  private Scanner sc;
+    /**
+     * Scanner for reading from console.
+     */
+    private Scanner sc;
 
-  public HighEffortLowEffort() {
-    this.sc = new Scanner(System.in);
-  }
+    public HighEffortLowEffort() {
+        this.sc = new Scanner(System.in);
+    }
 
-  /** @return - Returns the maximum among the 2 numbers */
-  private static int max(int x, int y) {
-    return (x > y ? x : y);
-  }
-
-  /** Method is for execute finding process. */
-  public void execute() {
     /**
      * @param days - quantity of days
      * @param high - range of max tasks
      * @param low - range of low tasks
      */
-    try {
-      System.out.println("Enter the period(in days):");
-      int days = sc.nextInt();
+    private int days;
+    private int[] high;
+    private int[] low;
 
-      System.out.println("Enter amount of the high effort task:");
-      int[] high = new int[days];
-
-      for (int i = 0; i < days; i++) {
-        high[i] = sc.nextInt();
-      }
-      System.out.println("Enter amount of the low effort task:");
-      int[] low = new int[days];
-
-      for (int i = 0; i < days; i++) {
-        low[i] = sc.nextInt();
-      }
-      /** An array task_dp that stores the maximum task done */
-      int[] task_dp = new int[days + 1];
-
-      /** If n = 0, no solution exists */
-      task_dp[0] = 0;
-
-      /** If n = 1, high effort task on that day will be the solution */
-      task_dp[1] = high[0];
-
-      /** Fill the entire array determining which task to choose on day i */
-      for (int i = 2; i <= days; i++) {
-        task_dp[i] = max(high[i - 1] + task_dp[i - 2], low[i - 1] + task_dp[i - 1]);
-        System.out.println(task_dp[days]);
-      }
-    } catch (Exception ex) {
-      System.out.println("You have to input an integer\n");
+    /**
+     * @return - Returns the maximum among the 2 numbers
+     */
+    private int max(int x, int y) {
+        return (x > y ? x : y);
     }
-  }
+
+    /**
+     * Request the input data statements in user:
+     * day,high tasks,low tasks
+     */
+    private void requestStatements() {
+        System.out.println("Enter the period(in days):");
+        days = sc.nextInt();
+
+        System.out.println("Enter amount of the high effort task:");
+        high = new int[days];
+
+        for (int i = 0; i < days; i++) {
+            high[i] = sc.nextInt();
+        }
+        System.out.println("Enter amount of the low effort task:");
+        low = new int[days];
+
+        for (int i = 0; i < days; i++) {
+            low[i] = sc.nextInt();
+        }
+    }
+
+    /**
+     * Returns maximum amount of task that can be
+     * done till days
+     */
+    private int maxTasks() {
+        // An array task_dp that stores the maximum
+        // task done
+        int[] task_dp = new int[days + 1];
+
+        // If days = 0, no solution exists
+        task_dp[0] = 0;
+
+        // If days = 1, high effort task on that day will
+        // be the solution
+        task_dp[1] = high[0];
+
+        // Fill the entire array determining which
+        // task to choose on day i
+        for (int i = 2; i <= days; i++)
+            task_dp[i] = Math.max(high[i - 1] + task_dp[i - 2],
+                    low[i - 1] + task_dp[i - 1]);
+        return task_dp[days];
+    }
+
+
+    /**
+     * Method is for execute finding process.
+     */
+    public void execute() {
+
+        try {
+            requestStatements();
+            System.out.println("Maximum amount of tasks you can perform within these " + days + " days is " + maxTasks());
+
+        } catch (Exception ex) {
+            System.out.println("You have to input an integer\n");
+        }
+    }
 }
