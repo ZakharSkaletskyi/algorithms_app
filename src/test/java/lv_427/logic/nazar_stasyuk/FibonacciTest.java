ac65b853acf6386f4437fade9ea1292fa6e8a6fe
fsociety01010 <alexbmx@mail.ua>
2019-07-16 13:07:12 +0300

    new test

---
+++ src/test/java/lv_427/logic/nazar_stasyuk/FibonacciTest.java
@@ -0,0 +1,35 @@
+package lv_427.logic.nazar_stasyuk;
+
+import mockit.Deencapsulation;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Fibonacci Test.
+ *
+ * @author Oleksandr Agarkov
+ * @version 1.0
+ */
+public class FibonacciTest {
+
+    private Fibonacci fibonacci;
+    /** Init method. */
+    @Before
+    public void init() {
+        fibonacci = new Fibonacci();
+    }
+    /** Method: findNumberByPosition(int position) has to a number 1 */
+    @Test
+    public void findNumberByPositionTest() {
+        long invoke = Deencapsulation.invoke(fibonacci, "findNumberByPosition", 100);
+        assertEquals(invoke,1);
+    }
+    /** Method: findNumberByPosition(int position) has to return 0 */
+    @Test
+    public void findNumberByPositionTestWithBadInput() {
+        long invoke = Deencapsulation.invoke(modificationFibonacci, "findNumberByPosition", -100);
+        assertEqquals(invoke,0);
+    }
+}
